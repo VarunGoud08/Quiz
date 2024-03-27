7ae949782cc08695ac178074bc91c92c0ce3fdff
VarunGoud08 <varungoud5520@gmail.com>
2024-03-27 15:54:42 +0530

    --migrations

--- src/main/resources/db/migration/V1__createTable.sql
+++ src/main/resources/db/migration/V1__createTable.sql
@@ -4,3 +4,15 @@ CREATE TABLE quizes (
     total_marks bigint NOT NULL,
     pass_marks bigint NOT NULL
 )
+
+CREATE TABLE questions (
+    id bigint DEFAULT nextval('questions_id'::regclass) PRIMARY KEY,
+    question_title character varying(255) NOT NULL,
+    option1 character varying(255) NOT NULL,
+    option2 character varying(255) NOT NULL,
+    option3 character varying(255) NOT NULL,
+    option4 character varying(255) NOT NULL,
+    right_answer character varying(255) NOT NULL,
+    difficulty_level character varying(255) NOT NULL,
+    category character varying(255) NOT NULL
+);
